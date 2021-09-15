76cb9556a81df9b39568499d54815f13fb9b0a4f
andrewmalty <90787290+andrewmalty@users.noreply.github.com>
2021-09-15 13:00:37 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,4 @@
-function subscribeUserToPush() {
-  return getSWRegistration()
-  .then(function(registration) {
-    const subscribeOptions = {
-      userVisibleOnly: true,
-      applicationServerKey: urlBase64ToUint8Array(
-        'BEl62iUYgUivxIkv69yViEuiBIa-Ib9-SkvMeAtA3LFgDzkrxZJjSgSnfckjBJuBkr3qBUYIHBQFLXYp5Nksh8U'
-      )
-    };
 
-    return registration.pushManager.subscribe(subscribeOptions);
-  })
-  .then(function(pushSubscription) {
-    console.log('Received PushSubscription: ', JSON.stringify(pushSubscription));
-    return pushSubscription;
-  });
-}
-subscribeUserToPush();
 self.addEventListener("pushsubscriptionchange", event => {
   document.getElementById("p1").innerHTML = event.oldSubscription;
   document.getElementById("p2").innerHTML = event.newSubscription;
