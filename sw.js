22d02d856c1be70aa7e71f7385b5b9a0d8f885b7
andrewmalty <90787290+andrewmalty@users.noreply.github.com>
2021-09-15 12:44:22 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-
+self.addEventListener("pushsubscriptionchange", event => {
+  document.getElementById("p1").innerHTML = event.oldSubscription;
+  document.getElementById("p2").innerHTML = event.newSubscription;
+},false);
